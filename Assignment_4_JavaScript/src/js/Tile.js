2aa2dad8cf76c9cc4f16e6d7a8cb84f90342a12d
Fady Ksho <fadyksho@gmail.com>
2019-03-02 22:47:51 +0100

    Add comment

--- Assignment_4_JavaScript/src/js/Tile.js
+++ Assignment_4_JavaScript/src/js/Tile.js
@@ -13,24 +13,29 @@ export class Tile {
     this[_initialzeTile](type);
   }
 
+  // the type of the tile never gonna change
   get type() {
     return this[_type];
   }
 
+  // the hostType is value('P' | 'B' | ' ') that the tile is holding
   set hostType(hostType) {
     this[_hostType] = hostType;
     // after the hostType change update the tile image
     this[_updateTileImage]();
   }
 
+  // get the hostType of the tile
   get hostType() {
     return this[_hostType];
   }
 
+  // get the image of hostType becouse thats what it will be render
   get tileImage() {
     return this[_tileImage];
   }
 
+  // check if this cell can contain a hostType
   get canContainHost() {
     return this[_hostType] === ' ';
   }
